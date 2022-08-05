f1560795f6bbc744a06de617e51088243e88cba3
mayank-del <mkjam.007@gmail.com>
2022-08-05 08:31:41 +0530

    aws-image-uploader-code-added

--- app.js
+++ app.js
@@ -7,11 +7,19 @@ require('dotenv').config()
 
 const app=express();
 const User=require("./models/User")
+//const expressfileupload=require("express-fileupload")
+
+const fs=require('fs')
+const util=require('util')
+const unlinkFile=util.promisify(fs.unlink)
+
+
 
 Connect_mongo();
 
 const user=require("./routes/user");
 const resource=require("./routes/resource");
+const {uploadFile,getFileStream}=require("./controllers/userController")
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -25,13 +33,32 @@ const imageStorage=multer.diskStorage({
     }
 });
 const upload=multer({
-    storage:imageStorage
-}).single('testImage');
+    dest:'uploads/'
+})
 
 app.use("/api/user",user);
 app.use("/api/resource",resource)
 
-app.post('/upload',(req,res)=>{
+app.get('/api/prof/:key',(req,res)=>{
+    const key=req.params.key
+    const readStream=getFileStream(key)
+
+    readStream.pipe(res)
+})
+
+app.post("/api/prof",upload.single('image'),async(req,res)=>{
+    const file=req.file
+    console.log(file)
+    const result=await uploadFile(file)
+    console.log(result)
+    await unlinkFile(file.path)
+    const description=req.body.description
+    res.send({imagePath:`/api/prof/${result.Key}`})
+})
+
+//not tested yet, little bit confused
+
+/* app.post('/upload',(req,res)=>{
     upload(req,res,(err)=>{
         if(err){
             console.log(err)
@@ -45,17 +72,14 @@ app.post('/upload',(req,res)=>{
                 lname:req.body.lname,
                 dob:req.body.dob,
                 password:req.body.password,
-                image:{
-                    data:req.file,
-                    contentType:'image/png'
-                }
+                image:`https://koa-prof.s3.ap-south-1.amazonaws.com/${req.file.key}`
             })
             User.save()
             .then(()=>res.status(200).send("Successfully Registered the User"))
             .catch((err)=>console.log(err))
         }
     })
-})
+}) */
 
 const port =process.env.PORT;
 
